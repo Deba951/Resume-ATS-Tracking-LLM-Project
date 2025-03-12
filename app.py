d12474336a42bbf5cffc3781206d4755774c20dd
DEBARGHYA CHAKRAVARTY <83878346+Deba951@users.noreply.github.com>
2025-03-12 15:01:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 # Gemini Pro Response
 def get_gemini_repsonse(input):
+    # Assigning the Model
     model=genai.GenerativeModel('gemini-pro')
     response=model.generate_content(input)
     return response.text
@@ -19,15 +20,17 @@ def get_gemini_repsonse(input):
 def input_pdf_text(uploaded_file):
     reader=pdf.PdfReader(uploaded_file)
     text=""
+    # Iterate through all the pages
     for page in range(len(reader.pages)):
         page=reader.pages[page]
+        # Extracting the text
         text+=str(page.extract_text())
     return text
 
 #Prompt Template --> The Better the prompt better is the result
 input_prompt="""
 Hey Act Like a skilled or very experienced ATS(Application Tracking System) with a deep understanding of the tech field, software engineering, data science, data analysis and big data engineering. Your task is to evaluate the resume based on the given job description.
-You must consider the job market is very competitive and you should provide the best assistance for improving their resumes. Assign the percentage Matching based on the Job description and the missing keywords with high accuracy.  
+You must consider the job market is very competitive and you should provide the best assistance for improving the resumes. Assign the percentage Matching based on the Job description and the missing keywords with high accuracy.  
 resume:{text}
 description:{jd}
 
@@ -35,7 +38,7 @@ I want the response in one single string having the structure
 {{"JD Match":"%","MissingKeywords:[]","Profile Summary":""}}
 """
 
-## streamlit app
+## Creating the Streamlit App
 st.title("Smart ATS")
 st.text("Improve Your Resume ATS")
 jd=st.text_area("Paste the Job Description")
